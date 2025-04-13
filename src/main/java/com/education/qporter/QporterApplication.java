502aef0697a8022b9109a5b76f9860a7d82320cf
five-sun <tyo1012@naver.com>
2025-04-13 18:24:29 +0900

    initial setting

---
+++ src/main/java/com/education/qporter/QporterApplication.java
@@ -0,0 +1,13 @@
+package com.education.qporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QporterApplication.class, args);
+	}
+
+}
