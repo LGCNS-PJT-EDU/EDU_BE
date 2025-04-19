ada408ea252f4cdcc84a2cceebed5b22b6069360
honey <rhkrehdgjs@naver.com>
2025-04-19 13:58:31 +0900

    chore : 패키지 초기 설정

--- src/main/java/com/education/takeit/global/config/WebConfig.java
+++ src/main/java/com/education/takeit/global/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.education.takeit;
+package com.education.takeit.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
