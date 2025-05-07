c6643aa3ba971ab73fef2cbd3a6d4e671cbd4c49
five-sun <tyo1012@naver.com>
2025-05-07 16:01:54 +0900

    EDU-106 feat: 사전평가 GET 요청 구현

---
+++ src/main/java/com/education/takeit/exam/controller/PreExamController.java
@@ -0,0 +1,31 @@
+package com.education.takeit.exam.controller;
+
+import com.education.takeit.exam.dto.ExamResponse;
+import com.education.takeit.exam.service.PreExamService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/exam")
+@RequiredArgsConstructor
+public class PreExamController {
+
+    private final PreExamService preExamService;
+
+    /**
+     * 사전 평가 GET 요청(10문제)
+     * @return
+     */
+    @GetMapping("/pre")
+    @Operation(summary = "사전 평가 문제 리스트 요청", description = "사전평가 문제 GET API")
+    public ResponseEntity<List<ExamResponse>> getPreExam() {
+        List<ExamResponse> result = preExamService.getPreExam();
+        return ResponseEntity.ok(result);
+    }
+}
