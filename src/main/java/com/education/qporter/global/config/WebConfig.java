0d0d1c687f108e6e9012e84da1113616c4271d63
honey <rhkrehdgjs@naver.com>
2025-05-08 17:21:41 +0900

    EDU-275 chore: codestyle 맞추기

--- src/main/java/com/education/qporter/global/config/WebConfig.java
+++ src/main/java/com/education/qporter/global/config/WebConfig.java
@@ -7,12 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**") // 모든 API 경로에 대해
-			.allowedOrigins("*") // 모든 origin 허용
-			.allowedMethods("*") // 모든 HTTP 메서드 허용
-			.allowedHeaders("*") // 모든 헤더 허용
-			.allowCredentials(false); // 인증정보 포함 안함
-	}
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry
+        .addMapping("/**") // 모든 API 경로에 대해
+        .allowedOrigins("*") // 모든 origin 허용
+        .allowedMethods("*") // 모든 HTTP 메서드 허용
+        .allowedHeaders("*") // 모든 헤더 허용
+        .allowCredentials(false); // 인증정보 포함 안함
+  }
 }
--- src/main/java/com/education/takeit/exam/controller/PreExamController.java
+++ src/main/java/com/education/takeit/exam/controller/PreExamController.java
@@ -4,13 +4,12 @@ import com.education.takeit.exam.dto.ExamResponse;
 import com.education.takeit.exam.service.PreExamService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/exam")
@@ -18,16 +17,17 @@ import java.util.List;
 @Tag(name = "PreExam", description = "사전 평가와 관련된 API")
 public class PreExamController {
 
-    private final PreExamService preExamService;
+  private final PreExamService preExamService;
 
-    /**
-     * 사전 평가 GET 요청(10문제)
-     * @return
-     */
-    @GetMapping("/pre")
-    @Operation(summary = "사전 평가 문제 리스트 요청", description = "사전평가 문제 GET API")
-    public ResponseEntity<List<ExamResponse>> getPreExam() {
-        List<ExamResponse> result = preExamService.getPreExam();
-        return ResponseEntity.ok(result);
-    }
+  /**
+   * 사전 평가 GET 요청(10문제)
+   *
+   * @return
+   */
+  @GetMapping("/pre")
+  @Operation(summary = "사전 평가 문제 리스트 요청", description = "사전평가 문제 GET API")
+  public ResponseEntity<List<ExamResponse>> getPreExam() {
+    List<ExamResponse> result = preExamService.getPreExam();
+    return ResponseEntity.ok(result);
+  }
 }
