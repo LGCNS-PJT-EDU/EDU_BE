8ea8f4f6d812cee27d46f3d9e4b09df2e8b2ff80
HSK021843 <9902kim@hufs.ac.kr>
2025-05-09 13:17:16 +0900

    EDU-251 chore: Swagger 적용을 위해 Controller 수정

--- src/main/java/com/education/takeit/feedback/controller/FeedbackController.java
+++ src/main/java/com/education/takeit/feedback/controller/FeedbackController.java
@@ -3,6 +3,9 @@ package com.education.takeit.feedback.controller;
 import com.education.takeit.feedback.dto.FeedbackResponse;
 import com.education.takeit.feedback.service.FeedbackService;
 import java.util.List;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,12 +13,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/feedback")
 @RequiredArgsConstructor
+@Tag(name = "피드백 ", description = "피드백 관련 API")
 public class FeedbackController {
 
   private final FeedbackService feedbackService;
 
   // 차후에 URI 변경 가능성 존재
   @GetMapping("/retrieve")
+  @Operation(summary = "특정 사용자 피드백 목록 조회", description = "Parameter로 주어진 userId와 일치하는 사용자의 모든 피드백 데이터를 JSON 형태로 반환")
   public ResponseEntity<List<FeedbackResponse>> findAllFeedback(@RequestParam String userId) {
     List<FeedbackResponse> list = feedbackService.findFeedback(userId);
     return ResponseEntity.ok(list);
