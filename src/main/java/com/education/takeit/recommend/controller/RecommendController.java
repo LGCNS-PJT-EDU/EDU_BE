c8377c1ab5c55eec25f08d792646b1274efbd885
binys21 <100662232+binys21@users.noreply.github.com>
2025-05-21 10:41:12 +0900

    EDU-358 feat: 추천 컨텐츠 조회

---
+++ src/main/java/com/education/takeit/recommend/controller/RecommendController.java
@@ -0,0 +1,30 @@
+package com.education.takeit.recommend.controller;
+
+import com.education.takeit.recommend.dto.UserContentResDto;
+import com.education.takeit.recommend.service.RecommendService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/recommend")
+@Tag(name = "추천 컨텐츠", description = "추천 컨텐츠 관련 API")
+public class RecommendController {
+
+  private final RecommendService recommendService;
+
+  @GetMapping
+  @Operation(summary = "추천받은 컨텐츠 조회", description = "사용자가 추천받은 컨텐츠 조회하는 API")
+  public ResponseEntity<List<UserContentResDto>> getUserContent(
+      @RequestParam("userId") Long userId) {
+    List<UserContentResDto> contentList = recommendService.getUserContent(userId);
+    return ResponseEntity.ok(contentList);
+  }
+}
---
+++ src/main/java/com/education/takeit/recommend/entity/TotalContent.java
@@ -0,0 +1,44 @@
+package com.education.takeit.recommend.entity;
+
+import com.education.takeit.roadmap.entity.LectureAmount;
+import com.education.takeit.roadmap.entity.PriceLevel;
+import com.education.takeit.roadmap.entity.Subject;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "totalContent")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class TotalContent {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long contentId;
+
+  @Column(name = "content_title", nullable = false)
+  private String contentTitle;
+
+  @Column(name = "content_url", nullable = false)
+  private String contentUrl;
+
+  @Column(name = "content_type", nullable = false)
+  private String contentType;
+
+  @Column(name = "content_platform", nullable = false)
+  private String contentPlatform;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "content_duration", nullable = false)
+  private LectureAmount contentDuration;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "content_price", nullable = false)
+  private PriceLevel contentPrice;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "sub_id", nullable = false)
+  private Subject subject;
+}
