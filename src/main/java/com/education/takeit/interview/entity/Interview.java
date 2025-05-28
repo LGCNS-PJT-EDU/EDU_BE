6419d5aac826e22b2775ffc2640b844e1703d6c4
binys21 <100662232+binys21@users.noreply.github.com>
2025-05-28 13:56:45 +0900

    EDU-413 feat: 면접 관련 엔티티 생성 및 DB 스크립트 생성

---
+++ src/main/java/com/education/takeit/interview/entity/Interview.java
@@ -0,0 +1,21 @@
+package com.education.takeit.interview.entity;
+
+import com.education.takeit.roadmap.entity.Subject;
+import jakarta.persistence.*;
+
+@Entity
+public class Interview {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long interviewId;
+
+    @Column(name = "interview_id", nullable = false)
+    private String interviewContent;
+
+    @Column(name="interview_answer", nullable = false)
+    private String interviewAnswer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sub_id", nullable = false)
+    private Subject subject;
+}
---
+++ src/main/java/com/education/takeit/interview/entity/UserInterviewReply.java
@@ -0,0 +1,21 @@
+package com.education.takeit.interview.entity;
+
+import com.education.takeit.user.entity.User;
+import jakarta.persistence.*;
+
+@Entity
+public class UserInterviewReply {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long replyId;
+
+    @Column(name = "user_reply", nullable = false)
+    private String userReply;
+
+    @Column(name = "interview_id",nullable = false)
+    private Long interviewId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
