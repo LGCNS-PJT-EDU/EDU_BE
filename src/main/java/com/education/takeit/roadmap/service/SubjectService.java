f5b4689f018089c33e6c73487832d4b199237e52
binys21 <100662232+binys21@users.noreply.github.com>
2025-05-30 13:02:44 +0900

    EDU-426 fix: 추천 컨텐츠 생성 요청 api 로 변경

--- src/main/java/com/education/takeit/roadmap/service/SubjectService.java
+++ src/main/java/com/education/takeit/roadmap/service/SubjectService.java
@@ -50,7 +50,7 @@ public class SubjectService {
         roadmapRepository.findBySubjectAndRoadmapManagement(subject, userRoadmapManagement);
 
     // 추천 컨텐츠 받아오기
-    List<UserContentResDto> recommendContents = recommendService.getUserContent(userId);
+    List<UserContentResDto> recommendContents = recommendService.fetchAndSaveRecommendation(userId, subjectId);
 
     // DTO화
     return new SubjectFindResDto(
