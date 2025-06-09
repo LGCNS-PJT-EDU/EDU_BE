b1b906ca85bd2ce5fc16c8acc020e1b75cd8b1a1
오썬 <tyo1012@naver.com>
2025-06-10 00:32:55 +0900

    EDU-489 fix: 단위 테스트에 적절하게 수정

--- src/test/java/com/education/takeit/TakeitApplicationTests.java
+++ src/test/java/com/education/takeit/TakeitApplicationTests.java
@@ -1,13 +1,12 @@
 package com.education.takeit;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
 @ActiveProfiles("test")
 class TakeitApplicationTests {
 
-  @Test
-  void contextLoads() {}
+    @Test
+    void contextLoads() {
+    }
 }
