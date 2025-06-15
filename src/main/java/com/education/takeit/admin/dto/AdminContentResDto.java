4544c2dbc1c9c9e59a3c37771664ed98f7f3ff35
HoGyeongC <ghdy98@gmail.com>
2025-06-15 17:51:17 +0900

    EDU-526 feat: 관리자 추천 컨텐츠 검색 기능 추가

---
+++ src/main/java/com/education/takeit/admin/dto/AdminContentResDto.java
@@ -0,0 +1,11 @@
+package com.education.takeit.admin.dto;
+
+public record AdminContentResDto(
+        Long totalContentId,
+        String contentTitle,
+        String contentUrl,
+        String contentType,
+        String contentPlatform,
+        String subName,
+        Long userCount
+) {}
